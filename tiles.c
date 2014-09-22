237140cfd8798ee0d29e6c4ef7180a9ccdb1e1b6
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2014-09-22 02:19:12 -0700

    Add animated tile drawing

--- tiles.c
+++ tiles.c
@@ -2,11 +2,19 @@
 
 #inctile_ex(tiles, "graphics/tile0.pcx", 0, 0, 1, 1, 1,\
             "graphics/tile1.pcx", 0, 0, 1, 1, 2);
+#incpal(tile0pal, "graphics/tile0.pcx");
+#incpal(tile1pal, "graphics/tile1.pcx");
 
-#incpal(tile0pal,"graphics/tile0.pcx");
-#incpal(tile1pal,"graphics/tile1.pcx");
+char game_timer;
+char x;
+char y;
+char tile_num;
 
 run() {
+    game_timer = 0;
+    x = 0;
+    y = 0;
+
     set_color_rgb(1, 7, 7, 7);
     set_font_color(1, 0);
     set_font_pal(0);
@@ -19,6 +27,29 @@ run() {
     load_palette(2, tile1pal, 1);
     put_tile(0, 0, 0);
     put_tile(1, 1, 0);
+
+    for (;;) {
+        if (x < 16) {
+            if (game_timer % 2 == 0) {
+                tile_num = 0;
+            } else {
+                tile_num = 1;
+            }
+            put_tile(tile_num, x, y);
+            x++;
+        } else {
+            x = 0;
+            y++;
+        }
+
+        if (y == 14) {
+            y = 0;
+        }
+
+        game_timer++;
+
+        vsync(0);
+    }
 }
 
 clear_display() {
